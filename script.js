e6fe70a0082400fbb68fe618798bdc48552c1bc1
Meharunnesa <meharunnesab28@gmail.com>
2025-04-09 21:52:39 +0600

    array-part-1

--- script.js
+++ script.js
@@ -32,3 +32,28 @@ const person = {firstname:'Meharunnesa', lastname:'Bristy', age:25};
 
 
 console.log(typeof cars);
+
+// array
+
+const car = ['Saab', 'Volvo', "BMW"];
+document.getElementById('array').innerHTML = car ;
+document.getElementById('array').innerHTML = car.length ;
+document.getElementById('array').innerHTML = car[ car.length - 1 ] ;
+
+console.log(typeof car);
+
+
+let alength = car.length ;
+let list = '<ul>';
+
+let i;
+
+for( i = 0 , i <= alength , i ++)
+{
+    list += '<li>' + car[i] + '</li>';
+}
+
+list += '</ul>';
+
+console.log(list);
+
