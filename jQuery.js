28715f60818428fdeda0ff29b11761c874f807c6
Meharunnesa <meharunnesab28@gmail.com>
2025-04-23 15:07:31 +0600

    chaining, get, set, add

--- jQuery.js
+++ jQuery.js
@@ -64,7 +64,7 @@ $(document).ready(function(){
         $('.fade-1').fadeIn();
         $('.fade-2').fadeIn('slow');
         $('.fade-3').fadeIn(3000);
-    })
+    });
 });
 
 // slide & stop()
@@ -72,11 +72,11 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.slide').click(function(){
         $('.slide-content').slideToggle(3000);
-    })
+    });
 
     $('.stop').click(function(){
         $('.slide-content').stop();
-    })
+    });
 });
 
 // animation
@@ -113,5 +113,66 @@ $(document).ready(function(){
             opacity : "1"
         }, "slow");
 
-    })   
+    }) ;  
+});
+
+// chaning
+
+$(document).ready(function(){
+    $('.chain-btn').click(function(){
+        $('.chain').css('background', 'gray').slideUp(3000).slideDown(3000).hide(3000);
+    });
+});
+
+// get
+
+$(document).ready(function(){
+    $('.btn-text').click(function(){
+        alert("Text: " + $('.get-text').text())
+    });
+
+    $('.btn-html').click(function(){
+        alert("Text: " + $('.get-text').html())
+    });
+
+    $('.btn-input').click(function(){
+        alert( $('.get-input').val() + "!!")
+    });
+
+    $('.btn-attr').click(function(){
+        alert( $('.get-attr').attr('href'))
+    });
+
+});
+
+// set
+
+$(document).ready(function(){
+    $('.set-text').click(function(){
+        $('.set-text-1').text("hello!!");
+    });
+
+    $('.set-html').click(function(){
+        $('.set-text-2').html("<h1>Hi</h1>");
+    });
+
+    $('.set-val').click(function(){
+        $('.set-text-3').val("Bristy");
+    });
+
+    $('.set-attr').click(function(){
+        $('.set-text-4').attr("href", "https://www.w3schools.com/jquery/jquery_dom_set.asp");
+    });
+});
+
+// add
+
+$(document).ready(function(){
+    $('.add-btn').click(function(){
+        $('.add-text').append("<b>hello</b>");
+    });
+
+    $('.add-btn').click(function(){
+        $('.add-text').prepend("<b>hi</b>");
+    });
 });
